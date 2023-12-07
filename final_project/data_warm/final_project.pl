9ee8cab60dd9ab79f34ecea113a78a39c40b1cbc
Al Ashir Intisar <109549706+Al-Ashir-Intisar@users.noreply.github.com>
2023-12-07 04:42:21 +0000

    extracted the tRNA, rRNA, CDS regions into xlsx

--- final_project/data_warm/final_project.pl
+++ final_project/data_warm/final_project.pl
@@ -8,7 +8,7 @@ my @genbank_files = (
     'Ursus maritimus.gb', 'Canis lupus familiaris.gb', 'Falco peregrinus.gb', 'Megaptera novaeangliae.gb', 'Psittacus erithacus.gb', 'Vulpes vulpes.gb'
 );
 
-print FH1 "file_name\tGenus\tSpecies\ttype\tmtDNA_size\tmtDNA\n";
+print FH1 "file_name\tGenus\tSpecies\tmtDNA_size\ttRNA_starts\ttRNA_ends\trRNA_starts\trRNA_ends\tCDS_starts\tCDS_ends\tmtDNA\n";
 foreach my $genbank_file (@genbank_files) {
     # Build the full path to the file
     my $full_path = "$genbank_file";
@@ -16,11 +16,10 @@ foreach my $genbank_file (@genbank_files) {
     open(GB, $full_path);
 
     while (my $record = IRS(GB)) {
-        my ($Genus, $Species, $Genome_size, $mtDNA) = separate($record);
+        my ($Genus, $Species, $Genome_size, $mtDNA, $tRNA_starts, $tRNA_ends, $rRNA_starts, $rRNA_ends, $CDS_starts, $CDS_ends) = separate($record);
 
-        if ($Genus && $Species && $Genome_size) {
-            print FH1 "$full_path\t$Genus\t$Species\twarm_blooded\t$Genome_size\t$mtDNA\n";
-            print FH2 "$mtDNA\n\n";
+        if ($Genus && $Species && $Genome_size && $mtDNA) {
+            print FH1 "$full_path\t$Genus\t$Species\t$Genome_size\t$tRNA_starts\t$tRNA_ends\t$rRNA_starts\t$rRNA_ends\t$CDS_starts\t$CDS_ends\t$mtDNA\n";
         }
     }
 
@@ -41,7 +40,46 @@ sub separate {
     
     my ($genus, $species) = ($record =~ /ORGANISM\s+(\w+)\s+(\w+)/s);
     my ($genome_size) = ($record =~ /LOCUS\s+\S+\s+(\d+)\s+bp/s);
-    my ($mtDNA) = ($record =~ /ORIGIN\s+(.*)\n\n/s);
+    my ($mtDNA) = ($record =~ /ORIGIN\s+(.*)\/\/\n\n/s);
     $mtDNA =~ s/[\d\s]//g;
-    return ($genus, $species, $genome_size, $mtDNA);
+    
+    my @tRNA_starts;
+    my @tRNA_ends;
+
+    while ($record =~ /tRNA\s+(\d+)\.\.(\d+)/sg) {
+        my ($start, $end) = ($1, $2);
+        push(@tRNA_starts, $start);
+        push(@tRNA_ends, $end);
+        #print "tRNA start: $start, tRNA end: $end\n";
+    }
+
+    my $tRNA_starts = join('+', @tRNA_starts); 
+    my $tRNA_ends = join('+', @tRNA_ends);
+
+    my @rRNA_starts;
+    my @rRNA_ends;
+    while ($record =~ /rRNA\s+(\d+)\.\.(\d+)/sg) {
+        my ($start, $end) = ($1, $2);
+        push(@rRNA_starts, $start);
+        push(@rRNA_ends, $end);
+        #print "rRNA start: $start, rRNA end: $end\n";
+    }
+    my $rRNA_starts = join('+', @rRNA_starts);
+    my $rRNA_ends = join('+', @rRNA_ends);
+
+    my @CDS_starts;
+    my @CDS_ends;
+    while ($record =~ /CDS\s+(\d+)\.\.(\d+)/sg) {
+        my ($start, $end) = ($1, $2);
+        push(@CDS_starts, $start);
+        push(@CDS_ends, $end);
+        #print "CDS start: $start, CDS end: $end\n";
+    }
+
+    my $CDS_starts = join('+', @CDS_starts);
+    my $CDS_ends = join('+', @CDS_ends);
+
+    #print @tRNA_starts, "\n"; #@tRNA_ends, "\n", @rRNA_starts, "\n", @rRNA_ends, "\n", @CDS_starts, "\n", @CDS_ends, "\n";
+
+    return ($genus, $species, $genome_size, $mtDNA, $tRNA_starts, $tRNA_ends, $rRNA_starts, $rRNA_ends, $CDS_starts, $CDS_ends);
 }
--- final_project/data_warm/try.pl
+++ final_project/data_warm/try.pl
@@ -1,12 +1,10 @@
-my $genbank_content = <<'END_CONTENT';
-ORIGIN      1     ggattaatga ctaatcagcc catgatcaca cataactgtg gtgtcatgca tttggtatct
-END_CONTENT
+# Initialize arrays for starts and ends
+my @starts = (1, 5, 10);
+my @ends = (3, 8, 13);
 
-# Extract the DNA sequence
-if ($genbank_content =~ /ORIGIN\s+(.*)\n+/s) {
-    my $dna_sequence = $1;
-    $dna_sequence =~ s/\s+//g;  # Remove remaining whitespace
-    print "DNA Sequence: $dna_sequence\n";
-} else {
-    print "DNA sequence not found in the GenBank content.\n";
-}
+# Create a list using references to the arrays
+my $list = { start => \@starts, end => \@ends };
+
+# Access the list elements
+print "Starts: ", join(", ", @{$list->{start}}), "\n";
+print "Ends: ", join(", ", @{$list->{end}}), "\n";
